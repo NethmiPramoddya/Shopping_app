d16a0f6dad1539dd8fe2fa9e2f9783d41ce5839a
NethmiPramoddya <nethmik2001@gmail.com>
2025-07-28 21:55:02 +0530

    Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import ProductCard from './components/productCard'
 import SuperProduct from './components/superProduct'
+import Homepage from './pages/homepage'
+import LoginPage from './pages/loginPage'
+import RegisterPage from './pages/registerPage'
+import AdminPage from './pages/adminPage'
 
 function App() {
   
 
   return (
     <>
-      <div>
-        <SuperProduct/>
+    <BrowserRouter>
+      <div className='w-full h-screen'>
+        <Routes path ="/">
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
+        <Route path='/admin/*' element={<AdminPage/>}/>
         {/* <ProductCard 
             name="Samsung galaxy S24 Ultra"
             price="$1900"
@@ -17,7 +27,9 @@ function App() {
             name="Apple MacBook Pro 16 inch"
             price="$2500"
             image ="https://picsum.photos/id/1/200/300"/> */}
+            </Routes>
       </div>
+      </BrowserRouter>
     </>
   )
 }
