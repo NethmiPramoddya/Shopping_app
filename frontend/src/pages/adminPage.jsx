f3e15ce018c19bc78266a4e1017bbd104c97b894
NethmiPramoddya <nethmik2001@gmail.com>
2025-07-28 22:07:46 +0530

    fixing bug(import react router dom to adminpage)

--- frontend/src/pages/adminPage.jsx
+++ frontend/src/pages/adminPage.jsx
@@ -1,10 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+
 export default function AdminPage() {
   return (
     <div className="w-full h-screen bg-red-900 flex">
       <div className="w-[300px] h-full bg-white">
 
       </div>
-      <div className="w-[calc(100%-300px)] h h-full bg-blue-900">
+      <div className="w-[calc(100%-300px)] h-full bg-blue-900">
         <Routes path="/*">
         <Route path = "/" element={<h1>Dashboard</h1>}/>
         <Route path = "/products" element={<h1>Products</h1>}/>
