c98da4c797843b02011e2a04e5dde69da312b92b
NethmiPramoddya <nethmik2001@gmail.com>
2025-09-05 00:25:03 +0530

    feature admin-securing created and feature logout enabled

--- frontend/src/pages/adminPage.jsx
+++ frontend/src/pages/adminPage.jsx
@@ -7,27 +7,65 @@ import ProductsAdminPage from "./admin/productsAdminPage";
 import AddProductAdminPage from "./admin/addProductAdminPage";
 import UpdateProductPage from "./admin/updateProductPage";
 import OrdersPageAdmin from "./admin/OrdersPageAdmin";
+import Loader from "../components/loader";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function AdminPage() {
+  const Navigate = useNavigate()
+  const[adminValidated, setAdminValidated] = useState(false)
+
+  useEffect(()=>{
+    const token = localStorage.getItem("token")
+    if(token==null){
+      toast.error("Please login first")
+      Navigate("/login")
+      return;
+    }else{
+      axios.get(import.meta.env.VITE_BACKEND_URL+"/api/users/",{
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }).then((response)=>{
+        if(response.data.role=="admin"){
+          setAdminValidated(true)
+        }else{
+          toast.error("You are not authorized")
+          Navigate("/login")
+        }
+      }).catch((error)=>{
+        toast.error("Something went wrong")
+        Navigate("/login")
+        console.log(error)
+      })
+    }
+  },[])
+
+
   return (
     <div className="w-full h-screen flex">
-      <div className="w-[300px] h-full bg-white flex flex-col items-center">
-        <span className="text-3xl font-bold my-5">Admin Panel</span>
-            <Link className="flex flex-row h-[60px] w-full border p-[20px] items-center text-xl gap-[25px]" to="/admin/products"><FaBoxArchive />Products</Link>
-            <Link className="flex flex-row h-[60px] w-full border p-[20px] items-center text-xl gap-[25px]" to="/admin/orders"><GiShoppingBag />Orders</Link>
-            <Link className="flex flex-row h-[60px] w-full border p-[20px] items-center text-xl gap-[25px]" to="/admin/users"><IoPeople />Users</Link>
-            <Link className="flex flex-row h-[60px] w-full border p-[20px] items-center text-xl gap-[25px]" to="/admin/settings"><IoSettingsSharp />Settings</Link>
+      {adminValidated?<>
+        <div className="w-[300px] h-full bg-white flex flex-col items-center">
+          <span className="text-3xl font-bold my-5">Admin Panel</span>
+              <Link className="flex flex-row h-[60px] w-full border p-[20px] items-center text-xl gap-[25px]" to="/admin/products"><FaBoxArchive />Products</Link>
+              <Link className="flex flex-row h-[60px] w-full border p-[20px] items-center text-xl gap-[25px]" to="/admin/orders"><GiShoppingBag />Orders</Link>
+              <Link className="flex flex-row h-[60px] w-full border p-[20px] items-center text-xl gap-[25px]" to="/admin/users"><IoPeople />Users</Link>
+              <Link className="flex flex-row h-[60px] w-full border p-[20px] items-center text-xl gap-[25px]" to="/admin/settings"><IoSettingsSharp />Settings</Link>
 
-      </div>
-      <div className="w-[calc(100%-300px)] h-full bg-amber-100">
-        <Routes path="/*">
-        <Route path = "/" element={<h1>Dashboard</h1>}/>
-        <Route path = "/products" element={<ProductsAdminPage/>}/>
-        <Route path = "/newProduct" element={<AddProductAdminPage/>}/>
-        <Route path = "/orders" element={<OrdersPageAdmin/>}/>
-        <Route path = "/updateProduct" element={<UpdateProductPage/>}/>
-        </Routes>
-      </div>
+        </div>
+        <div className="w-[calc(100%-300px)] h-full bg-amber-100">
+          <Routes path="/*">
+          <Route path = "/" element={<h1>Dashboard</h1>}/>
+          <Route path = "/products" element={<ProductsAdminPage/>}/>
+          <Route path = "/newProduct" element={<AddProductAdminPage/>}/>
+          <Route path = "/orders" element={<OrdersPageAdmin/>}/>
+          <Route path = "/updateProduct" element={<UpdateProductPage/>}/>
+          </Routes>
+        </div>
+      </>:<Loader />}
     </div>
   )
 }
